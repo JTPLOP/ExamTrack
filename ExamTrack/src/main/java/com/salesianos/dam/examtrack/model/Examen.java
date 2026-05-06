f7b1d02ea177e9f99e6b87d89f66c9fbffb0052e
Jose <jotaplop.07j@gmail.com>
2026-05-06 22:24:16 +0200

    feat: desarrollo del menu lateral como fragmento además de su estilo correspondiente

--- ExamTrack/src/main/java/com/salesianos/dam/examtrack/model/Examen.java
+++ ExamTrack/src/main/java/com/salesianos/dam/examtrack/model/Examen.java
@@ -13,7 +13,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
--- ExamTrack/src/main/java/com/salesianos/dam/examtrack/model/Profesor.java
+++ ExamTrack/src/main/java/com/salesianos/dam/examtrack/model/Profesor.java
@@ -11,7 +11,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter @Setter @EqualsAndHashCode @ToString
