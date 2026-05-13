4e9dbc706cb72e5ae2134730ebb31bdbae03d9e0
Manolo <tapia.lojos25@triana.salesianos.edu>
2026-05-13 12:39:20 +0200

    refactor: se reajusta la estructuras de servicio creando una base generica para todas las entidades

---
+++ ExamTrack/src/main/java/com/salesianos/dam/examtrack/service/ExamenServicio.java
@@ -0,0 +1,27 @@
+package com.salesianos.dam.examtrack.service;
+
+import org.springframework.stereotype.Service;
+import com.salesianos.dam.examtrack.model.Examen;
+import com.salesianos.dam.examtrack.repository.ExamenRepositorio;
+import com.salesianos.dam.examtrack.service.base.ServicioBaseImpl;
+
+@Service
+public class ExamenServicio extends ServicioBaseImpl <Examen, Long, ExamenRepositorio> {
+
+    public ExamenServicio(ExamenRepositorio repositorio) {
+        super(repositorio);
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+}
