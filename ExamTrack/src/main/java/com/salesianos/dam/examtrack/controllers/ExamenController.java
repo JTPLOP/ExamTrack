23808830ff8a02bda24d05f662009c1f1d29a3b0
Manolo <tapia.lojos25@triana.salesianos.edu>
2026-05-13 10:18:58 +0200

    feat: diseño formulario de creación de examenes; se agregan controllers y nuevo endpoint de acceso directo

--- ExamTrack/src/main/java/com/salesianos/dam/examtrack/controllers/ExamenController.java
+++ ExamTrack/src/main/java/com/salesianos/dam/examtrack/controllers/ExamenController.java
@@ -3,6 +3,7 @@ package com.salesianos.dam.examtrack.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ExamenController {
@@ -13,5 +14,20 @@ public class ExamenController {
         return "examenes";
     }
 
+
+    @GetMapping ("/formExamen") 
+    public String formularioExamen () {
+
+        return "formExamenes";
+    }
+
+    @PostMapping ("/crearExamen") 
+    public String creadorExamen (Model model) {
+
+
+
+        System.out.println("Hola mundo");
+        return "redirect:/formExamen";
+    }
     
 }
