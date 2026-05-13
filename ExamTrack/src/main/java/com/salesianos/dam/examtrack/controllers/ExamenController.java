3cbd0c70807e1b12056c5bd232cf8ce1917babde
Manolo <tapia.lojos25@triana.salesianos.edu>
2026-05-13 13:26:25 +0200

    feat: se configura en controller junto a la lógica para crear examenes

--- ExamTrack/src/main/java/com/salesianos/dam/examtrack/controllers/ExamenController.java
+++ ExamTrack/src/main/java/com/salesianos/dam/examtrack/controllers/ExamenController.java
@@ -3,10 +3,19 @@ package com.salesianos.dam.examtrack.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.salesianos.dam.examtrack.model.Examen;
+import com.salesianos.dam.examtrack.service.ExamenServicio;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class ExamenController {
+
+    private final ExamenServicio servicio;
     
     @GetMapping ("/examenes")
     public String misExamenesBase (Model model) {
@@ -22,11 +31,14 @@ public class ExamenController {
     }
 
     @PostMapping ("/crearExamen") 
-    public String creadorExamen (Model model) {
+    public String creadorExamen (@ModelAttribute("examen") Examen examen, Model model) {
 
+        servicio.agregar(examen);
 
+        /*Comprobacion de creacion objeto */
+        System.out.println("EXAMEN CREADO");
+        System.out.println(examen.toString());
 
-        System.out.println("Hola mundo");
         return "redirect:/formExamen";
     }
     
