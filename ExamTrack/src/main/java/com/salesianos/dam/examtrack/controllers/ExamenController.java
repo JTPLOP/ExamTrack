a5f6fd0a084f2325d077093c14c7be2eb74f139d
Manolo <tapia.lojos25@triana.salesianos.edu>
2026-05-18 14:26:20 +0200

    refactor: formulario de edicion de examenes

--- ExamTrack/src/main/java/com/salesianos/dam/examtrack/controllers/ExamenController.java
+++ ExamTrack/src/main/java/com/salesianos/dam/examtrack/controllers/ExamenController.java
@@ -1,9 +1,12 @@
 package com.salesianos.dam.examtrack.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.salesianos.dam.examtrack.model.Examen;
@@ -24,26 +27,44 @@ public class ExamenController {
         model.addAttribute("examen", servicio.filtrarTodos());
 
         return "examenes";
-    }
+    } 
 
 
     @GetMapping ("/formExamen") 
-    public String formularioExamen () {
+    public String formularioExamen (Model model) {
 
         return "formExamenes";
     }
 
-    @PostMapping ("/crearExamen") 
+    @PostMapping ("/examenSubmit") 
     public String creadorExamen (@ModelAttribute("examen") Examen examen, Model model) {
 
+        
         servicio.agregar(examen);
 
         /*Comprobacion de creacion objeto */
         System.out.println("EXAMEN CREADO");
         System.out.println(examen.toString());
 
-        return "redirect:/formExamen";
+        return "redirect:/examenes";
     }
+
+    @GetMapping("/editar/examen/{id}")
+	public String modificarExamen(@PathVariable("id") Long id, Model model) {
+
+		Optional <Examen> examen = servicio.filtrarPorId(id);
+
+		if (examen.isPresent()) {
+            model.addAttribute("examen", servicio.filtrarPorId(id));
+
+			return "formExamenes";
+		} else {
+			return "redirect:/examenes";
+		}
+
+	}
+
+
 
     
     
