f8af70c5505cd8ebfd14a9cc1fa43a3af481c4a7
Jose <jotaplop.07j@gmail.com>
2026-05-18 20:28:28 +0200

    refactor: refactorizacion formulario alumnos y se crean rutas para la edicion

--- ExamTrack/src/main/java/com/salesianos/dam/examtrack/controllers/AlumnoController.java
+++ ExamTrack/src/main/java/com/salesianos/dam/examtrack/controllers/AlumnoController.java
@@ -1,11 +1,15 @@
 package com.salesianos.dam.examtrack.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.salesianos.dam.examtrack.model.Alumno;
+import com.salesianos.dam.examtrack.model.Examen;
 import com.salesianos.dam.examtrack.service.AlumnoServicio;
 
 import lombok.RequiredArgsConstructor;
@@ -39,6 +43,29 @@ public class AlumnoController {
         alumno.depurarDatos();
 
         return "redirect:/formAlumno";
+    }
+
+    @GetMapping("/editar/alumno/{dni}")
+	public String modificarAlumno(@PathVariable("dni") String dni, Model model) {
+
+		Optional <Alumno> alumno = servicio.filtrarPorId(dni); // aprender a filtrar por dni (como hacer consultas)
+
+		if (alumno.isPresent()) {
+            model.addAttribute("alumno", alumno.get());
+
+			return "formAlumnos";
+		} else {
+			return "redirect:/alumnos";
+		}
+
+	}
+
+    @PostMapping ("/editAlumno")
+    public String editorAlumno (@ModelAttribute("examen") Alumno alumno) {
+        
+        servicio.modificar(alumno);
+
+        return "redirect:/alumnos";
     }
 
 
