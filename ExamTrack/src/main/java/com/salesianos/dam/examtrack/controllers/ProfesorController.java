193024bc379c988dac32c854f84e9ec009e415ce
Manolo <tapia.lojos25@triana.salesianos.edu>
2026-05-20 14:21:14 +0200

    feat: se agrega logica para la edicion de atributos del profesor

--- ExamTrack/src/main/java/com/salesianos/dam/examtrack/controllers/ProfesorController.java
+++ ExamTrack/src/main/java/com/salesianos/dam/examtrack/controllers/ProfesorController.java
@@ -1,9 +1,12 @@
 package com.salesianos.dam.examtrack.controllers;
 
+import java.util.Optional;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
 import com.salesianos.dam.examtrack.model.Alumno;
@@ -65,7 +68,28 @@ public class ProfesorController {
     }
 
 
+    @GetMapping("/editar/profesor/{dni}")
+	public String modificarAlumno(@PathVariable("dni") String dni, Model model) {
 
+		Optional <Profesor> profesor = servicio.filtrarPorId(dni); // aprender a filtrar por dni (como hacer consultas)
+
+		if (profesor.isPresent()) {
+            model.addAttribute("profesor", profesor.get());
+
+			return "formProfesor";
+		} else {
+			return "redirect:/profesores";
+		} 
+
+	}
+
+    @PostMapping ("/editProfesor")
+    public String editorAlumno (@ModelAttribute("profesor") Profesor profesor) {
+        
+        servicio.modificar(profesor);
+
+        return "redirect:/profesores";
+    }
     
 
 
