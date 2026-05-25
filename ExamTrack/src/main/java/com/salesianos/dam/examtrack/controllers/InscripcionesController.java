ba649126698a57940c635b1ddab6da884c884471
Jose Tapia <jotaplop.07j@gmail.com>
2026-05-25 22:18:31 +0200

    feat: se agrega controller de inscripción y boton de redireccion hacia la vista

---
+++ ExamTrack/src/main/java/com/salesianos/dam/examtrack/controllers/InscripcionesController.java
@@ -0,0 +1,34 @@
+package com.salesianos.dam.examtrack.controllers;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.salesianos.dam.examtrack.model.Examen;
+import com.salesianos.dam.examtrack.service.ExamenServicio;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class InscripcionesController {
+
+    private final ExamenServicio examService;
+
+    @GetMapping("/examen/inscripciones/{id}")
+    public String verInscripciones(@PathVariable("id") Long id, Model model) {
+        Optional<Examen> examen = examService.filtrarPorId(id);
+        
+        if (examen.isPresent()) {
+            model.addAttribute("examen", examen.get());
+
+            return "inscripciones";
+        } else {
+            return "redirect:/examenes";
+        }
+    }
+
+}
