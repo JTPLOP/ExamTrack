4ca30c59ec277053221ef79c32269550ede6054e
Manolo <tapia.lojos25@triana.salesianos.edu>
2026-05-26 12:34:50 +0200

    feat: se agrega listado de alumnos a las inscripciones

--- ExamTrack/src/main/java/com/salesianos/dam/examtrack/controllers/InscripcionesController.java
+++ ExamTrack/src/main/java/com/salesianos/dam/examtrack/controllers/InscripcionesController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.salesianos.dam.examtrack.model.Examen;
+import com.salesianos.dam.examtrack.service.AlumnoServicio;
 import com.salesianos.dam.examtrack.service.ExamenServicio;
 
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 public class InscripcionesController {
 
     private final ExamenServicio examService;
+    private final AlumnoServicio alumServicio;
 
     @GetMapping("/examen/inscripciones/{id}")
     public String verInscripciones(@PathVariable("id") Long id, Model model) {
@@ -24,6 +26,7 @@ public class InscripcionesController {
         
         if (examen.isPresent()) {
             model.addAttribute("examen", examen.get());
+            model.addAttribute("alumno", alumServicio.filtrarTodos());
 
             return "inscripciones";
         } else {
