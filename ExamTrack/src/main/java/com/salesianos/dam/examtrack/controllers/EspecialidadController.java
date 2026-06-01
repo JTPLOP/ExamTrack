3f9afcbea1d9cf37cbbba56605a449e4ad3773b1
Manolo <tapia.lojos25@triana.salesianos.edu>
2026-06-01 14:07:03 +0200

    feat: se agrega funcionalidad de modificar la especialidad

--- ExamTrack/src/main/java/com/salesianos/dam/examtrack/controllers/EspecialidadController.java
+++ ExamTrack/src/main/java/com/salesianos/dam/examtrack/controllers/EspecialidadController.java
@@ -46,5 +46,31 @@ public class EspecialidadController {
         return "redirect:/especialidades";
     }
 
+    @GetMapping("/editar/especialidad/{id}")
+    public String modificarEspecialidad(@PathVariable("id") Long id, Model model) {
+
+        Optional<Especialidad> especialidad = servicio.filtrarPorId(id);
+
+        if (especialidad.isPresent()) {
+            model.addAttribute("especialidad", especialidad.get());
+            return "formEspecialidad";
+        } else {
+            return "redirect:/especialidades";
+        }
+    }
+
+    @PostMapping("/editEspecialidad")
+    public String editorEspecialidad(@Valid @ModelAttribute("especialidad") Especialidad especialidad, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "formEspecialidad";
+        }
+
+        servicio.modificar(especialidad);
+
+        return "redirect:/especialidades";
+    }
+
     
+
 }
