d319f68dfa97c73b0b4cb4b22de6ac2338a60245
Manolo <tapia.lojos25@triana.salesianos.edu>
2026-06-01 13:13:08 +0200

    feat: se agrega funcionalidad de modificar un departamento

--- ExamTrack/src/main/java/com/salesianos/dam/examtrack/controllers/DepartamentoController.java
+++ ExamTrack/src/main/java/com/salesianos/dam/examtrack/controllers/DepartamentoController.java
@@ -1,10 +1,13 @@
 package com.salesianos.dam.examtrack.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.salesianos.dam.examtrack.model.Departamento;
@@ -40,7 +43,32 @@ public class DepartamentoController {
 
         servicio.agregar(departamento);
 
-        return "redirect:/formDepartamento";
+        return "redirect:/departamentos";
+    }
+
+    @GetMapping("/editar/departamento/{id}")
+    public String modificarDepartamento(@PathVariable("id") Long id, Model model) {
+
+        Optional<Departamento> departamento = servicio.filtrarPorId(id);
+
+        if (departamento.isPresent()) {
+            model.addAttribute("departamento", departamento.get());
+            return "formDepartamento";
+        } else {
+            return "redirect:/departamentos";
+        }
+    }
+
+    @PostMapping("/editDepartamento")
+    public String editorDepartamento(@Valid @ModelAttribute("departamento") Departamento departamento, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "formDepartamento";
+        }
+
+        servicio.modificar(departamento);
+
+        return "redirect:/departamentos";
     }
 
 }
